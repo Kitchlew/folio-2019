4892c1821008598e01482463b483102f690209eb
Shehreyar Kitchlew <62876048+Kitchlew@users.noreply.github.com>
2023-04-11 09:51:40 +0500

    Revert "Changes for i-CAMP"
    This reverts commit b5b9930e3fde61545f94cb9991a96d331daad6f2.

--- src/javascript/Materials/Floor.js
+++ src/javascript/Materials/Floor.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three'
-import { Color } from 'three'
 
 import shaderFragment from '../../shaders/floor/fragment.glsl'
 import shaderVertex from '../../shaders/floor/vertex.glsl'
